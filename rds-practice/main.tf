e45e46d955f9755f3672f8193c627c51b7fed3ac
ASHISH RANJAN <ashishranjan@ASHISHs-MacBook-Air.local>
2025-11-04 21:28:05 +0530

    rds

--- rds-practice/main.tf
+++ rds-practice/main.tf
@@ -1,13 +1,28 @@
-resource "aws_db_instance" "db" {
+resource "aws_db_instance" "db1" {
     identifier = "rds-test"
     engine = "mysql"
-    engine_version = "8.40"
-    db_name = "database-1"
+    engine_version = "8.0"
+    db_name = "database1"
     instance_class = var.instance_class
-    storage_type = 
-
-    depends_on = [ aws_db_subnet_group.name ]
+    allocated_storage = 10
+    username = "admin"
+    password = "ashish111111"
+    parameter_group_name = "default.mysql8.0"
+    skip_final_snapshot = true
+    publicly_accessible = false
+    db_subnet_group_name = aws_db_subnet_group.dbsubnet.name
+    backup_retention_period = 7
+    apply_immediately = true
+    depends_on = [ aws_db_subnet_group.dbsubnet ]
     
+}
+
+resource "aws_db_instance" "readreplica" {
+    identifier = "read-replica"
+    replicate_source_db = aws_db_instance.db1.identifier
+    instance_class = var.instance_class
+    publicly_accessible = false
+    depends_on = [ aws_db_instance.db1 ]
 }
 
 data "aws_subnet" "pub-1" {
@@ -25,11 +40,11 @@ data "aws_subnet" "pub-2" {
   }
 }
 
-resource "aws_db_subnet_group" "name" {
-    name = "subnet-group"
+resource "aws_db_subnet_group" "dbsubnet" {
+    name = "subnet-group-1"
     subnet_ids = [ data.aws_subnet.pub-1.id,data.aws_subnet.pub-2.id ]
     tags = {
-      Name = "subnet Group"
+      Name = "subnet Group-1"
     }
   
 }
